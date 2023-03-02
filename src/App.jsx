e22d2248580cffbe62450cc7750ae6b4d059aa82
Estanislao Rey <tankysoluciones@gmail.com>
2023-03-02 00:07:32 -0300

    ultimas modificaciones del codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import NavBar from './componentes/NavBar/NavBar'
-import {CartContextProvider} from './Context/CartContext'
-import {ItemListContainer} from './componentes/ItemList/ItemListContainer'
+import { CartContextProvider } from './Context/CartContext'
+import { ItemListContainer } from './componentes/ItemList/ItemListContainer'
 import ItemDetailContainer from './componentes/ItemDetailContainer/ItemDetailContainer'
-import Error from './componentes/Error/error'
-
 import CartContainer from './componentes/CartContainer/CartContainer'
 import SignIn from './componentes/signIn/signIn'
+import Error from './componentes/Error/error'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
@@ -21,9 +20,8 @@ function App() {
           <Route path='/category/:id' element={<ItemListContainer />} />
           <Route path='/item/:id' element={<ItemDetailContainer />} />
           <Route path='/cart' element={<CartContainer />} />
+          <Route path='/signin' element={<SignIn />} />
           <Route path='*' element={<Error />} />
-          <Route path='/signin' element={<SignIn/>} />
-          {/* <Route path='*' element={<Navigate to='/error' />} /> */}
         </Routes>
       </CartContextProvider>
     </BrowserRouter>
--- src/componentes/ItemList/ItemListContainer.jsx
+++ src/componentes/ItemList/ItemListContainer.jsx
@@ -3,13 +3,14 @@ import { getFirestore, where, query, collection, getDocs } from 'firebase/firest
 import { useParams } from "react-router-dom"
 import ItemList from "../ItemList/ItemList"
 import Loading from "../Spiner/Loading"
+import Error from "../Error/error"
 
 export const ItemListContainer = () => {
   const [products, setProducts] = useState([])
   const [loading, setLoading] = useState(true)
   const { id } = useParams()
 
-   useEffect(() => {
+  useEffect(() => {
     const db = getFirestore()
     const queryCollection = collection(db, 'items')
     const queryFilter = id ? query(queryCollection, where('category', '==', id)) : queryCollection; //ordeno querry para poder filtrar y aplico sugar
@@ -20,10 +21,14 @@ export const ItemListContainer = () => {
       .finally(() => setLoading(false))
   }, [id])
 
-    return (
+  return (
     loading ? <Loading /> : (
-      products !== null ? <ItemList products={products} /> : <p>Hubo un error al cargar los productos.</p>
+      (products !== null) ? <ItemList products={products} />
+        : (
+          <Error />
+        )
     )
   )
 }
 export default ItemListContainer
+
